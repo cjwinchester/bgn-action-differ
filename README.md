ad04d96c98e870c7043835a3362765814b0b6b4e
Cody Winchester <cjwinchester@gmail.com>
2018-12-25 12:27:18 -0700

    first up

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# bgn-action-differ
-Check for new items on the U.S. Geological Survey Board of Domestic Names action list.
+# Board on Geographic Names action list tracker
+The U.S. Geological Survey's [Board on Geographic Names](https://geonames.usgs.gov/) is a public body whose job is to "to maintain uniform geographic name usage throughout the Federal Government." The board also rules on requests by the public to rename geographic features, including those with [offensive names](https://politics.myajc.com/news/state--regional-govt--politics/feds-still-await-georgia-request-change-name-runaway-negro-creek/9P20jHWusJmrFnxPSTQoXO/).
+
+This repo contains some Python that does three things:
+1. Download the latest version of [the BGN's "Action list" Excel file with name change proposals](https://geonames.usgs.gov/domestic/quarterly_list.htm), which also includes board actions within the past 12 months.
+2. Find and print any diffs between that file and the most recently downloaded version of the file.
+3. Incrementally add records to a CSV that keeps track of board actions.
+
+## Running the script
+I'm managing my dependencies -- `requests`, `BeautifulSoup`, `pandas` and `xlrd` -- with `pipenv`, but do whatever makes sense for your setup.
+
+- Clone or download this repo
+- `cd` into the directory and run `pipenv install` (or whatever your dependency management setup is)
+- `pipenv run python bgn_action.py` (or whatever etc.)
---
+++ bgn_action.py
@@ -0,0 +1,141 @@
+import datetime
+import glob
+import os
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+CSV_FILE = 'latest.csv'
+
+
+def get_xl_link():
+    '''
+    Parse the BGN page to get a link to the latest action list file.
+
+    Returns:
+        A tuple with the updated date and a link to the Excel file.
+     '''
+
+    URL = 'https://geonames.usgs.gov/apex/f?p=geonames_web:review_lists'
+
+    # grab the page
+    r = requests.get(URL)
+
+    # turn it into soup
+    soup = BeautifulSoup(r.text, 'html.parser')
+
+    # find the correct link
+    link = soup.find('a', text='Action List')
+
+    # grab the container span with the date info
+    span = link.parent.text.split(':')[1].strip().rstrip(')')
+
+    # parse out the date in iso format
+    updated_date = datetime.datetime.strptime(
+        span,
+        '%B %d, %Y').date().isoformat()
+
+    # return a tuple with the date and the link
+    return (updated_date, 'https://geonames.usgs.gov/apex/' + link['href'])
+
+
+def download_latest_xl(updated, link):
+    '''
+    Given an updated date and a link, download the latest Excel file.
+
+    Returns:
+        The absolute path to the downloaded file.
+    '''
+
+    # format the filename with the date updated
+    filename = f'{updated}-bgn-action-list.xls'
+
+    # if this file doesn't exist already
+    if not os.path.isfile(filename):
+
+        # fetch the link
+        r = requests.get(link)
+
+        # and write the contents to file
+        with open(os.path.join('raw', filename), 'wb') as f:
+            for block in r.iter_content(1024):
+                f.write(block)
+
+    # return absolute path to the file
+    return os.path.abspath(filename)
+
+
+def diff_latest_and_write():
+    '''
+    1. Find the differences between the most recent file and the next
+       most recent file
+    2. Write out a CSV file with the latest unique records
+
+    Returns a list of dictionaries of new records.
+    '''
+
+    # get a sorted list of existing Excel files
+    XLS_FILES = sorted(glob.glob('raw/*.xls'))
+
+    # get name of latest file
+    latest_file = XLS_FILES[-1]
+
+    # read latest file into a data frame
+    df_latest = pd.read_excel(latest_file)
+
+    # if main CSV file doesn't exist already
+    if not os.path.exists(CSV_FILE):
+        # write out the latest data frame to file
+        df_latest.to_csv(CSV_FILE, index=False)
+    # if it exists already
+    else:
+        # read the CSV into a data frame
+        main_df = pd.read_csv(CSV_FILE)
+
+        # combine it with the latest data frame
+        new_main_df = pd.concat([df_latest, main_df], axis=1)
+
+        # drop duplicate records
+        new_main_df.drop_duplicates()
+
+        # write back out to main CSV
+        new_main_df.to_csv(CSV_FILE, index=False)
+
+    # bail if there's only one file in the directory
+    if len(XLS_FILES) == 1:
+        print('I only found one file.')
+        return None
+
+    # but if there are multiple files
+    else:
+        # grab the name of the second most recent file
+        next_latest_file = XLS_FILES[-2]
+
+        # read it into a data frame
+        df_next_latest = pd.read_excel(next_latest_file)
+
+        # combine it with the latest file in a new data frame
+        # drop duplicates and keep none of them
+        diff = pd.concat(
+            [df_latest, df_next_latest]
+        ).drop_duplicates(keep=False)
+
+        # return diff records as a list of dictionaries
+        return diff.to_dict('records')
+
+
+if __name__ == '__main__':
+
+    # grab the link
+    latest = get_xl_link()
+
+    # download the latest file
+    download_latest_xl(*latest)
+
+    # find the diffs and write out to file
+    diffs = diff_latest_and_write()
+
+    # print diffs
+    print(diffs)
